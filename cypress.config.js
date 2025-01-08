097d8fb7016da08fee07afc2f4c0644e76840c1a
Gabriella Costa <gabriella.lemos.costa@gmail.com>
2025-01-08 15:56:51 -0300

    setimo commit cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require("cypress");
+const mochawesome = require("mochawesome");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Relatório Mochawesome
+      on('after:run', (results) => {
+        mochawesome.create(results);
+      });
+
+      // Configuração do Cypress Cloud (projectId)
+      config.projectId = "q6wu8b";
+
+      return config;
+    },
+    reporter: "mochawesome", // Usa o Mochawesome como reporter
+    reporterOptions: {
+      reportDir: "cypress/reports", // Diretorio onde o relatório será salvo
+      overwrite: true,
+      html: true,
+      json: true
     },
   },
 });
